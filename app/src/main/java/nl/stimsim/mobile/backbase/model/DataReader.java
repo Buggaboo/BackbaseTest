6eba40259a3b79e6873b0bf685cf0323b385e8e8
Jasm Sison <jasm.sison@gmail.com>
2018-03-27 19:41:15 +0200

    clean up

--- app/src/main/java/nl/stimsim/mobile/backbase/model/DataReader.java
+++ app/src/main/java/nl/stimsim/mobile/backbase/model/DataReader.java
@@ -4,41 +4,11 @@ import com.google.gson.stream.JsonReader;
 
 import java.io.IOException;
 
-// import android.util.JsonReader; // This one isn't mocked of course, gotta use GSON
-
 /**
  * Created by jasmsison on 27/03/2018.
  */
 
 public class DataReader {
-
-    // probably the worst performing implementation, it should work though, albeit slowly
-    /*
-    @Deprecated
-    public void fromJSONArray(final CoordinateTrie root, final JSONArray array) {
-        for(int i=0; i<array.length(); i++) {
-            try {
-                JSONObject o = (JSONObject) array.get(i);
-                String country = o.getString("country"); // TODO group as Map<String, Set<CoordTrie>>
-                String name = o.getString("name");
-                JSONObject coord = o.getJSONObject("coord");
-                double lon = coord.getDouble("lon");
-                double lat = coord.getDouble("lat");
-
-                root.buildTrie(name, lon, lat);
-            } catch (JSONException e) {
-                Log.getStackTraceString(e);
-            }
-        }
-    }
-
-    /// TODO move this to the Application/Activity
-    public void fromRawJsonReader(final CoordinateTrie root, Context context, @RawRes int rawResId) throws IOException {
-        Resources resources = context.getResources();
-        InputStream is = resources.openRawResource(rawResId);
-        fromJsonReader(root, is);
-    }
-    */
 
     public void fromJsonReader(final CoordinateTrie root, final JsonReader reader) throws IOException {
         reader.beginArray();
@@ -78,5 +48,5 @@ public class DataReader {
         reader.endArray();
     }
 
-    // TODO RegExp implementation... later, if time allows
+    // TODO RegExp implementation... later, if time allows, I doubt it will be faster and more correct...
 }
